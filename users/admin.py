1440e87bff734aff0b8dcf437651291657264f97
aliounemane2 <MANE@pc2411.home>
2018-01-17 21:34:15 +0000

    HFC WEB

---
+++ users/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+
+from users.models import *
+
+
+# class UserAdmin(admin.ModelAdmin):
+class UserAdmin(ImportExportModelAdmin):
+    list_display = ('nom', 'prenom', 'email')
+    search_fields = ('telephone', 'email')
+    list_filter = ('telephone',)
+
+
+
+
+
+class PaiementAdmin(ImportExportModelAdmin):
+    list_display = ('date_debut', 'date_fin',)
+
+
+class Type_AbonnementAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'montant',)
+    search_fields = ('nom', 'montant')
+    list_filter = ('nom',)
+
+
+admin.site.register(utilisateur2, UserAdmin)
+admin.site.register(Paiement, PaiementAdmin)
+admin.site.register(Type_Abonnement, Type_AbonnementAdmin)
+
+admin.site.site_title = 'Administration de la plateforme HFC'
+admin.site.site_header = 'Administration de la plateforme HFC'
+
+
---
+++ users/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+
+
+class utilisateur2(models.Model):
+
+    civilite = models.CharField(max_length=100, null=True, blank=True)
+    prenom = models.CharField(max_length=150)
+    nom = models.CharField(max_length=150)
+    dateNaiss = models.CharField(max_length=22, null=True, blank=True)
+    lieuNaiss = models.CharField(max_length=130, null=True, blank=True)
+    adresse = models.CharField(max_length=175, null=True, blank=True)
+    ville = models.CharField(max_length=150, null=True, blank=True)
+    email = models.CharField(max_length=150, null=True, blank=True)
+    profession = models.CharField(max_length=125, null=True, blank=True)
+    prenom1 = models.CharField(max_length=125, null=True, blank=True)
+    nom1 = models.CharField(max_length=125, null=True, blank=True)
+    telephone1 = models.CharField(max_length=40, null=True , blank=True)
+    prenom2 = models.CharField(max_length=125, null=True, blank=True)
+    nom2 = models.CharField(max_length=125, null=True, blank=True)
+    telephone2 = models.CharField(max_length=125, null=True, blank=True)
+    idcarte = models.IntegerField()
+    telephone = models.CharField(max_length=50, blank=True, null=True)
+    id_typeabonnement = models.IntegerField()
+    photo = models.TextField(null=True, blank=True)
+    # status_email = models.CharField(max_length=1, blank=True, null=True)
+    status_email = models.CharField(max_length=1, default='1')
+
+
+    def __str__(self):
+        return str(self.telephone)
+
+
+
+class Type_Abonnement(models.Model):
+
+    nom = models.CharField(max_length=175, null=True, blank=True)
+    montant = models.IntegerField()
+
+    def __str__(self):
+        return str(self.nom)
+
+
+
+
+class Paiement(models.Model):
+    idPaiement = models.AutoField(primary_key=True)
+    date_debut = models.CharField(max_length=22, null=True, blank=True)
+    date_fin = models.CharField(max_length=22, null=True, blank=True)
+    idUser = models.IntegerField()
+    nbseances_en_cours = models.IntegerField(default=0)
+    nbseances_total = models.IntegerField(default=0)
+    dateencours = models.CharField(max_length=22,  null=True, blank=True)
+    date_autorisation = models.CharField(max_length=22, null=True, blank=True)
+
+
+    def __str__(self):
+        return str(self.idPaiement)
